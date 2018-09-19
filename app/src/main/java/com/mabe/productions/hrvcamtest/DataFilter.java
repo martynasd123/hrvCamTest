982638255bb9896d7e3a64c5a9425b3b0efa077d
martynasd123 <dargis.martynas@gmail.com>
2018-09-19 13:44:00 +0300

    initial

---
+++ app/src/main/java/com/mabe/productions/hrvcamtest/DataFilter.java
@@ -0,0 +1,30 @@
+package com.mabe.productions.hrvcamtest;
+
+import java.util.ArrayList;
+
+public class DataFilter {
+/*
+
+    public double[] bandPass(double lowPass, double highPass, Complex[] values){
+
+        double [] freqArray = new double[values.length];
+        for(int i = 0; i<values.length; i++){
+            freqArray[i] = (double)i/(double)values.length;
+        }
+
+        ArrayList<Complex> filteredArr = new ArrayList<>();
+
+        for(int i = 0; i < freqArray.length; i++){
+            if(freqArray[i] < highPass && freqArray[i] > lowPass){
+                filteredArr.add(values[i]);
+            }
+        }
+
+
+
+
+
+    }
+
+    */
+}
